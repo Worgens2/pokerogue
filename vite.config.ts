20fe494ec6aa575d3c38ad296005f983cf6aae13
Worgens2 <sleepyappaeats@gmail.com>
2025-08-14 19:55:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,9 @@
-import { defineConfig, loadEnv, type Rollup, type UserConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { minifyJsonPlugin } from "./src/plugins/vite/vite-minify-json-plugin";
+import { defineConfig } from 'vite';
 
-export const defaultConfig: UserConfig = {
-  plugins: [tsconfigPaths(), minifyJsonPlugin(["images", "battle-anims"], true)],
-  clearScreen: false,
-  appType: "mpa",
+export default defineConfig({
+  base: './',  // ensures the build works on any static host
   build: {
-    chunkSizeWarningLimit: 10000,
-    minify: "esbuild",
-    sourcemap: false,
-    rollupOptions: {
-      onwarn(warning: Rollup.RollupLog, defaultHandler: (warning: string | Rollup.RollupLog) => void) {
-        // Suppress "Module level directives cause errors when bundled" warnings
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        defaultHandler(warning);
-      },
-    },
-  },
-};
-
-export default defineConfig(({ mode }) => {
-  const envPort = Number(loadEnv(mode, process.cwd()).VITE_PORT);
-
-  return {
-    ...defaultConfig,
-    base: "",
-    esbuild: {
-      pure: mode === "production" ? ["console.log"] : [],
-      keepNames: true,
-    },
-    server: {
-      port: !Number.isNaN(envPort) ? envPort : 8000,
-    },
-  };
+    outDir: 'dist'
+  }
 });
+
